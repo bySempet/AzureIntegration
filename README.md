b8780c8722503ebb48d99afee5ee397d0ac96b61
Ion Olaizola Galan <82670931+bySempet@users.noreply.github.com>
2026-09-26 12:45:01 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # AzureIntegration
+Anotaciones Importantes durante el proceso de generación del state local, es decir, la generacion de la infraestructura que almacenara el state del resto del proyecto. State local de infra que almacena el state cloud.
+
+Elementos necesarios, resource_group, storage_account y storage_container. Las storage_accounts dependen del resource_grup y el container del storage_account. Para que se generen siempre en el orden necesario,
+se debe de definir con la siguiente nomenclatura "type.definedResourceName.variable", en la variable que necesita de dependencia.
+
+Always "terraform init" -> "terraform validate" -> "terraform plan" -> check what is going to be created and lastly -> "terraform apply".
+
+If any resource has been correctly created during the apply but the creation of the config hasnt been complete, you can check with "terraform state list" what has been created and what not. Then "terraform destroy" to eliminate those resources
+
+In new accounts for Azure, sometimes theres a problem with the Regions you can acces to. For example i used West Europe and didnt let me create some resources there. That is why i changed it to Spain Central.
+
+Im encounting this problem where the Storage Account is trying to be created but the resource_group cant me access for some reason. Then this resource is being taged as *tainted*, whis means is corrupted for terraform, so the next time you want to apply it is going to be recreated -> destroyed and created.
--- main.tf
+++ main.tf
@@ -4,12 +4,21 @@ terraform {
             version = "~> 5.0"
             source = "hashicorp/azurerm"
         }
-    }
+        random = {
+          source  = "hashicorp/random"
+          version = "~> 3.9"
+        }
+        time = {
+          source  = "hashicorp/time"
+          version = "~> 0.13"
+        }
+
+      }
     required_version = ">= 1.10"
 }
 
 locals {
-    location = "West Europe"
+    location = "North Europe"
     tags = {
         project    = "azure-lab"
         purpose    = "terraform-state"
@@ -21,6 +30,11 @@ provider "azurerm" {
     resource_providers_to_register = ["Microsoft.Storage"]
     features {}
 }
+resource "random_string" "suffix" {
+  length  = 6
+  special = false 
+  upper   = false 
+}
 
 resource "azurerm_resource_group" "rg" {
   name     = "rg-tfstate"
@@ -29,7 +43,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "sa"  {
-    name = "firststorageaccount"
+    name = "sttfstate${random_string.suffix.result}"
     resource_group_name = azurerm_resource_group.rg.name
     location = azurerm_resource_group.rg.location
     account_tier = "Standard"
@@ -37,11 +51,16 @@ resource "azurerm_storage_account" "sa"  {
     tags = local.tags
 }
 
+resource "time_sleep" "wait_for_storage_account" {
+  depends_on      = [azurerm_storage_account.sa]
+  create_duration = "90s"
+}
+
 resource "azurerm_storage_container" "tfstate" {
   name                  = "tfstate"
   storage_account_id    = azurerm_storage_account.sa.id
   container_access_type = "private"
-  tags = local.tags
+  depends_on = [time_sleep.wait_for_storage_account]
 }
 
 
